5b98a24424f483b750feb11ce9f7b1732e559603
Magnus Aagaard <jaaag17@student.sdu.dk>
2020-11-03 16:07:14 +0100

    Sensor fusion test ready

--- autonomous_ws/src/sensor_fusion/nodes/sensor_fusion.py
+++ autonomous_ws/src/sensor_fusion/nodes/sensor_fusion.py
@@ -13,14 +13,21 @@ import matplotlib.pyplot as plt
 class SensorFusion:
     def __init__(self):
         self.setup_subs()
+        self.dt = 0.02
         #First three is GPS, next 3 is IMU acc, next 3 is IMU gyro
         self.data_idx = np.zeros(9, dtype=np.int8)
         self.lat = 0
         self.lon = 0
         self.alt = 0
-        self.linear_acc = 0
-        self.angular_vel = 0
-        self.xhat = np.zeros(3)
+        self.u = np.zeros((3,1))
+        self.y = np.zeros((3,1))
+        self.xhat = np.zeros((3,1))
+        self.sigma = np.identity(self.xhat.shape[0])*10000
+        self.Q = np.identity(self.u.shape[0])
+        self.R = np.identity(self.y.shape[0])*0.02
+        self.C = np.identity(self.y.shape[0])
+
+        self.setup_state_model()
         
     def setup_subs(self):
         self.pos_sub = rospy.Subscriber('/mavros/global_position/global', NavSatFix, self.pos_cb)
@@ -31,25 +38,45 @@ class SensorFusion:
         self.lon = msg.longitude
         self.alt = msg.altitude
         self.data_idx[:3] = 1
+        self.y[:3] = np.asarray([msg.latitude, msg.longitude, msg.altitude]).reshape(3,1)
 
     def imu_cb(self, msg):
-        self.linear_acc = msg.linear_acceleration
-        self.angular_vel = msg.angular_velocity
+        self.u[:3] = np.asarray([msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z]).reshape(3,1)
+        #self.u[3:] = np.asarray([msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z])
         self.data_idx[3:9] = 1
 
+    def setup_state_model(self):
+        self.A = np.identity(self.xhat.shape[0])
+        #self.A[0][3] = self.dt
+        #self.A[1][4] = self.dt
+        #self.A[2][5] = self.dt
+        B1 = np.identity(3)*0.5*self.dt*self.dt
+        self.B = B1
+        #B2 = np.identity(3)*self.dt
+        #self.B = np.concatenate((B1,B2))
+
+
     def filter(self):
+        print("filter")
         #Run Kalman filter
+
         #prediction step
-        self.xhat = self.xhat + 
+        self.xhat = self.A.dot(self.xhat)+self.B.dot(self.u)
+        self.sigma = np.matmul(np.matmul(self.A, self.sigma),np.transpose(self.A)) + self.Q
         #update step
+        tmp = np.linalg.inv(np.matmul(np.matmul(self.C, self.sigma), np.transpose(self.C)) + self.R)
+        K = np.matmul(np.matmul(self.sigma, np.transpose(self.C)),tmp)
+        self.xhat = self.xhat + np.matmul(K,(self.y - np.matmul(self.C, self.xhat)))
+        self.sigma = np.matmul((np.identity(self.xhat.shape[0]) - np.matmul(K,self.C)),self.sigma)
 
 
     
 
 if __name__ == "__main__":
+    rospy.init_node('sensor_fusion_node', anonymous=True)
     sf = SensorFusion()
     rate = rospy.Rate(50)
     while not rospy.is_shutdown():
-        rospy.spin_once()
         sf.filter()
         rate.sleep()
+    rospy.spin()
